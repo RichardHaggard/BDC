7a113678ef4935bebe8633f7cb222dfdd2c45132
Richard Haggard <haggard@msn.com>
2019-03-23 20:43:18 -0400

    Added a base ViewModel.

---
+++ BDC_V1/BDC_V1/ViewModels/ShellViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BDC_V1.ViewModels
+{
+    public class ShellViewModel : ViewModelBase
+    {
+        // **************** Class enumerations ********************************************** //
+
+        // **************** Class data members ********************************************** //
+
+        // **************** Class properties ************************************************ //
+
+        public string LabelContent
+        {
+            get { return _LabelContent; }
+            set
+            {
+                if (_LabelContent != value)
+                {
+                    _LabelContent = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+        private string _LabelContent;
+
+
+        // **************** Class constructors ********************************************** //
+
+        /// <summary>
+        /// Default class constructor.
+        /// </summary>
+        public ShellViewModel()
+        {
+            LabelContent = "Something to confirm ShellViewModel is properly bound.";
+        }
+
+        // **************** Class members *************************************************** //
+
+
+    }
+}
---
+++ BDC_V1/BDC_V1/ViewModels/ViewModelBase.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace BDC_V1.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        // **************** Class enumerations ********************************************** //
+
+        // **************** Class data members ********************************************** //
+
+        // **************** Class properties ************************************************ //
+
+        // **************** Class constructors ********************************************** //
+
+        // **************** INotifyPropertyChanged implementation *************************** //
+
+        /// <summary>
+        /// This is the data member that external clients will subscribe to in order to
+        /// receive property change notifications.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        /// <summary>
+        /// This method is called by the Set accessor of each property. 
+        /// The CallerMemberName attribute that is applied to the optional propertyName
+        /// parameter causes the property name of the caller to be substituted as an argument.
+        /// </summary>
+        /// <param name="propertyName"></param>
+        protected void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    
+        
+        // **************** Class members *************************************************** //
+
+
+    }
+}
