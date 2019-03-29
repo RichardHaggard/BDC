0873cd80b8240219ad7fd4ed233bb5c58a8c2686
Richard Haggard <haggard@msn.com>
2019-03-28 21:30:09 -0400

    Interim update of the Inventory Details.

--- BDC_V1/BDC_V1/ViewModels/InventoryDetailsViewModel.cs
+++ BDC_V1/BDC_V1/ViewModels/InventoryDetailsViewModel.cs
@@ -6,7 +6,53 @@ using System.Threading.Tasks;
 
 namespace BDC_V1.ViewModels
 {
-    class InventoryDetailsViewModel
+    public class InventoryDetailsViewModel : ViewModelBase
     {
+        // **************** Class enumerations ********************************************** //
+
+        // **************** Class data members ********************************************** //
+
+        // **************** Class properties ************************************************ //
+
+        public string CurrentSection
+        {
+            get { return _CurrentSection; }
+            set { SetProperty(ref _CurrentSection, value); }
+        }
+        private string _CurrentSection;
+
+
+        public string DetailSelector
+        {
+            get { return _DetailSelector; }
+            set { SetProperty(ref _DetailSelector, value); }
+        }
+        private string _DetailSelector;
+
+
+        public List<string> DetailSelectors
+        {
+            get { return _DetailSelectors; }
+        }
+        private List<string> _DetailSelectors = new List<string>();
+
+        // **************** Class constructors ********************************************** //
+
+        public InventoryDetailsViewModel()
+        {
+            CurrentSection = "11057 - EAST BAY - D501003 INTERIOR DISTRIBUTION SERVICES dry-type, 480V primary 120/208V secondary, 225kVA";
+
+            DetailSelectors.Add("FL2 - TL12412C - GE - Rm207 - DAMAGED - The nameplate on component was missing ...");
+            DetailSelectors.Add("AB3 - GH504xx - AP - Rm111 - DAMAGE - Scratched and defaced");
+            DetailSelectors.Add("XY8 - OP583C - PD - Rm456 - SOMET - In very poor taste");
+
+            DetailSelector = "FL2 - TL12412C - GE - Rm207 - DAMAGED - The nameplate on component was missing ...";
+
+        }
+
+
+        // **************** Class members *************************************************** //
+
+
     }
 }
