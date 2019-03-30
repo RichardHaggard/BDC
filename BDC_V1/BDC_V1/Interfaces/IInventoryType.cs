5417726f7a565951eb740a28e65779ec0ec53854
BillDuffy <bill_duffy@hotmail.com>
2019-03-30 04:10:29 -0400

    Added a datagrid and some fake data to the QaInventory view.

---
+++ BDC_V1/BDC_V1/Interfaces/IInventoryType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BDC_V1.Interfaces
+{
+    public interface IInventoryType
+    {
+        string FacilityId  { get; set; }
+        string SystemId    { get; set; }
+        string ComponentId { get; set; }
+        string TypeName    { get; set; }
+        string SectionName { get; set; }
+        string InventIssue { get; set; }
+    }
+}
--- BDC_V1/BDC_V1/ViewModels/QaInventoryViewModel.cs
+++ BDC_V1/BDC_V1/ViewModels/QaInventoryViewModel.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BDC_V1.Classes;
+using BDC_V1.Interfaces;
+using BDC_V1.ViewModels;
+using Prism.Mvvm;
 
 namespace BDC_V1.ViewModels
 {
@@ -13,10 +18,35 @@ namespace BDC_V1.ViewModels
         // **************** Class data members ********************************************** //
 
         // **************** Class properties ************************************************ //
+        public ObservableCollection<IInventoryType> InventoryInfo { get; } =
+            new ObservableCollection<IInventoryType>();
 
         // **************** Class constructors ********************************************** //
+        public QaInventoryViewModel()
+        {
+            InventoryInfo.Add(new InventoryType()
+            {
+                FacilityId = "11057",
+                SystemId = "D30",
+                ComponentId = "D3010",
+                TypeName = "",
+                SectionName = "",
+                InventIssue = "Missing Section"
+            });
+
+            InventoryInfo.Add(new InventoryType()
+            {
+                FacilityId = "11057",
+                SystemId = "D30",
+                ComponentId = "D3010",
+                TypeName = "D302001",
+                SectionName = "N/A",
+                InventIssue = "Missing Photo"
+            });
+
+            InventoryInfo.AddRange(Enumerable.Repeat(new InventoryType(), 30));
+        }
 
         // **************** Class members *************************************************** //
-
     }
 }
