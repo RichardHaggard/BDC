e44c4383f30fbebf05d7a75b5c414dae15072ed0
Richard Haggard <haggard@msn.com>
2019-04-14 17:26:34 -0400

    Adding a model to contain Photo info.

---
+++ BDC_V1/BDC_V1/Models/PhotoModel.cs
@@ -0,0 +1,59 @@
+﻿using BDC_V1.Classes;
+
+namespace BDC_V1.Models
+{
+    public class PhotoModel : PropertyBase
+    {
+        // **************** Class enumerations ********************************************** //
+
+        // **************** Class data members ********************************************** //
+
+        // **************** Class properties ************************************************ //
+
+        public string Description
+        {
+            get { return _Description; }
+            set { SetProperty(ref _Description, value); }
+        }
+        public string _Description;
+
+
+        public string Filename
+        {
+            get { return _Filename; }
+            set { SetProperty(ref _Filename, value); }
+        }
+        public string _Filename;
+
+
+        public string Title
+        {
+            get { return _Title; }
+            set { SetProperty(ref _Title, value); }
+        }
+        public string _Title;
+
+
+        // **************** Class constructors ********************************************** //
+
+        public PhotoModel()
+        {
+
+        }
+
+
+        public PhotoModel(string filename, string title, string description)
+            : this()
+        {
+            Filename = filename;
+            Title = title;
+            Description = description;
+        }
+
+
+        // **************** Class members *************************************************** //
+
+
+    }
+}
+
