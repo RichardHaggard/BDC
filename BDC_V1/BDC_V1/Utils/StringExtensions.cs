dbc82063e7f3ed6f12871215c582d690c5084389
BillDuffy <bill_duffy@hotmail.com>
2019-04-29 04:03:05 -0400

    Modified the foreground colors of the Inspection Button array when nothing is selected.

--- BDC_V1/BDC_V1/Utils/StringExtensions.cs
+++ BDC_V1/BDC_V1/Utils/StringExtensions.cs
@@ -26,11 +26,17 @@ namespace BDC_V1.Utils
 
         public static bool IsLike(this string src, string pat)
         {
-            return string.IsNullOrEmpty(src) || string.IsNullOrEmpty(pat) ||
-                   src.Equals    (pat, StringComparison.InvariantCultureIgnoreCase) ||
-                   src.StartsWith(pat, StringComparison.InvariantCultureIgnoreCase) ||  // like 'pat%'
-                   src.EndsWith  (pat, StringComparison.InvariantCultureIgnoreCase) ||  // like '%pat'
-                   src.Contains  (pat, StringComparison.InvariantCultureIgnoreCase);    // like '%pat%'
+            if (string.IsNullOrEmpty(pat)) return true;     // if the pattern is empty, then we are "like" it.
+            if (string.IsNullOrEmpty(src)) return false;    // takes care of null / empty source strings when pattern is not
+
+            return src.StartsWith(pat, StringComparison.InvariantCultureIgnoreCase) ||  // like 'pat%' start here as it's the most likely
+                   src.Contains  (pat, StringComparison.InvariantCultureIgnoreCase); // like '%pat%'
+
+                   // I think StartsWith takes care of this
+                   // src.Equals   (pat, StringComparison.InvariantCultureIgnoreCase) ||  // like 'pat'
+
+                   // I think Contains takes care of this
+                   //src.EndsWith  (pat, StringComparison.InvariantCultureIgnoreCase) ||  // like '%pat'
         }
     }
 }
