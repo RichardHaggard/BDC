5f18ee1f6c62527fa095112662343a5fbf944261
BillDuffy <bill_duffy@hotmail.com>
2019-06-20 21:29:33 -0400

    Updating location stuff

--- BDC_V1/BDC_V1/App.xaml.cs
+++ BDC_V1/BDC_V1/App.xaml.cs
@@ -36,10 +36,10 @@ namespace BDC_V1
             new ReadOnlyDictionary<string, string>(
                 new Dictionary<string, string>
                 {
-                    {nameof(BuilderDcName)    , @"BuilderDC"},
+                    {nameof(BuilderDcPath)    , @"Cardno\<BuilderDcName>"},
                     {nameof(PathCalcTemplates), @"<PathBuilderDc>\CalculatorTemplates"},
                     {nameof(FileBuilderData)  , @"<PathBuilderDc>\<BuilderDcName>.mdb"},
-                    {nameof(PathLogFiles)     , @"<UserAppData>\Logs"},
+                    {nameof(PathLogFiles)     , @"<PathUserAppData>\Logs"},
                     {nameof(PathProjectFolder), @"<PathUserFolder>\<Project>"},
                     {nameof(PathNamedProject) , @"<PathUserFolder>\<ProjectIdName>"},
                     {nameof(PathBredFiles)    , @"<PathNamedProject>\<BREDName>"},
@@ -51,27 +51,17 @@ namespace BDC_V1
                     {nameof(FileBredData)	  , @"<PathProjectFolder>\<BREDFilename>"},
                     {nameof(FileBredPackage)  , @"<PathProjectFolder>\<PackageFilename>"},
                     {nameof(FileConfigData)   , @"<PathProjectFolder>\<ConfigFilename>"},
-                    {nameof(LookupFilename)   , @"" },
-                    {nameof(InspectorName)    , @"" },
-                    {nameof(BREDFilename)     , @"" },
-                    {nameof(PackageFilename)  , @"" },
-                    {nameof(ConfigFilename)   , @"" },
-                    {nameof(PathUserFolder)   , @"" },
-                    {nameof(Project)          , @"" },
-                    {nameof(ProjectIdName)    , @"" },
-                    {nameof(BREDName)         , @"" },
-                    {nameof(BuilderDcProgram) , @"" },
-                    {nameof(PathBuilderDc)    , @"" },
-                    {nameof(PathApplication)  , @"" },
-                    {nameof(UserAppData)      , @"" },
                 });
 
         // TODO: These names aren't quite working yet !!!
         // Paths that are constant for all users
         [NotNull] public static string BuilderDcName     { get => GetPath(); set => SetPath(value); }
+        [NotNull] public static string BuilderDcPath     { get => GetPath(); set => SetPath(value); }
         [NotNull] public static string BuilderDcProgram  { get => GetPath(); set => SetPath(value); }
-        [NotNull] public static string UserAppData       { get => GetPath(); set => SetPath(value); }
+        [NotNull] public static string PathUserAppData   { get => GetPath(); set => SetPath(value); }
         [NotNull] public static string PathApplication   { get => GetPath(); set => SetPath(value); }
+        [NotNull] public static string PathUserApp       { get => GetPath(); set => SetPath(value); }
+        [NotNull] public static string PathProgramData   { get => GetPath(); set => SetPath(value); }
         [NotNull] public static string PathBuilderDc     { get => GetPath(); set => SetPath(value); }
         [NotNull] public static string PathCalcTemplates { get => GetPath(); set => SetPath(value); }
         [NotNull] public static string FileBuilderData   { get => GetPath(); set => SetPath(value); }
@@ -102,101 +92,6 @@ namespace BDC_V1
         [NotNull] public static string Project           { get => GetPath(); set => SetPath(value); }
         [NotNull] public static string ProjectIdName     { get => GetPath(); set => SetPath(value); }
         [NotNull] public static string BREDName          { get => GetPath(); set => SetPath(value); }
-
-        private static string GetPath(
-            [CanBeNull, CallerMemberName] string propertyName = null)
-        {
-            return (string.IsNullOrEmpty(propertyName) || (_decoderDictionary == null))
-                ? string.Empty 
-                : _decoderDictionary[propertyName];
-        }
-
-        private static bool SetPath([NotNull] string value,
-            [CanBeNull, CallerMemberName] string propertyName = null)
-        {
-            if (string.IsNullOrEmpty(propertyName)) return false;
-
-            if ((_decoderDictionary != null) && (_decoderDictionary[propertyName] == value))
-                return false;
-
-            // build a list of all affected macros
-            var hitList = new List<string>();
-
-            if (_decoderDictionary == null)
-            {
-                _decoderDictionary = new Dictionary<string, string>(MasterDictionary);
-                hitList.AddRange(_decoderDictionary.Keys);  // mark all changed
-            }
-            else
-            {
-                // find which macros will be affected
-                hitList.Add(propertyName);
-
-                for (var modified = true; modified; )
-                {
-                    modified = false;
-
-                    foreach (var kvp in MasterDictionary
-                        .Where(kvp => !hitList.Contains(kvp.Key)))
-                    {
-                        foreach (var key in hitList)
-                        {
-                            var macro = "<" + key + ">";
-
-                            if (kvp.Value.Contains(macro))
-                            {
-                                hitList.Add(kvp.Key);
-                                modified = true;
-                                break;
-                            }
-                        }
-
-                        if (modified) break;
-                    }
-                }
-
-                foreach (var key in hitList
-                    .Where(key => key != propertyName))
-                {
-                    _decoderDictionary[key] = MasterDictionary[key];
-                }
-            }
-
-            // modify the specified value
-            _decoderDictionary[propertyName] = value;
-
-            // make a copy of the hit list for reporting
-            IReadOnlyList<string> notifyList = new List<string>(hitList);
-
-            for (var mod = true; mod; )
-            {
-                mod = false;
-
-                foreach (var key in hitList
-                    .Where(key => _decoderDictionary[key] != MasterDictionary[key]))
-                {
-                    var macro = "<" + key + ">";
-
-                    for (var modified = true; modified; )
-                    {
-                        modified = false;
-
-                        foreach (var kvp in _decoderDictionary 
-                            .Where(kvp => (kvp.Key != key) && 
-                                          kvp.Value.Contains(macro)))
-                        {
-                            _decoderDictionary[kvp.Key] =
-                                kvp.Value.Replace(macro, _decoderDictionary[key]);
-
-                            mod = modified = true;
-                            break;
-                        }
-                    }
-                }
-            }
-
-            return true;
-        }
 
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
         {
@@ -229,15 +124,32 @@ namespace BDC_V1
 
             // C:\Program Files (x86)\Cardno\BuilderDC
             PathApplication = System.IO.Path.GetDirectoryName(szApplication) ?? string.Empty;
+            Debug.Assert(System.IO.Directory.Exists(PathApplication));
 
-            // C:\ProgramData\Cardno\BuilderDC
-            PathBuilderDc = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
+            // set the name of the program folder
+            BuilderDcName = "BuilderDC";
 
-            // C:\Users\<UserName>\AppData\Roaming\Cardno\BuilderDC
-            UserAppData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            // C:\ProgramData
+            PathProgramData = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
+            Debug.Assert(System.IO.Directory.Exists(PathProgramData));
+
+            // C:\Users\<UserName>\AppData\Roaming
+            PathUserApp = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            Debug.Assert(System.IO.Directory.Exists(PathUserApp));
 
             // C:\Users\<UserName>\Documents
-            PathUserFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            // Do not use this, returns OneDrive folder if connected
+            //PathUserFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            PathUserFolder = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\Documents";
+            Debug.Assert(System.IO.Directory.Exists(PathUserFolder));
+
+            // C:\ProgramData\Cardno\BuilderDC
+            PathBuilderDc = PathProgramData + @"\" + BuilderDcPath;
+            System.IO.Directory.CreateDirectory(PathBuilderDc);     // create it if it doesn't exist
+
+            // C:\Users\<UserName>\AppData\Roaming\Cardno\BuilderDC
+            PathUserAppData = PathUserApp + @"\" + BuilderDcPath;
+            System.IO.Directory.CreateDirectory(PathUserAppData);     // create it if it doesn't exist
 
             PresentationTraceSources.Refresh();
             //PresentationTraceSources.DataBindingSource.Listeners.Add(new ConsoleTraceListener());
@@ -306,6 +218,110 @@ namespace BDC_V1
 
             return null;
         }
+
+        private static string GetPath(
+            [CanBeNull, CallerMemberName] string propertyName = null)
+        {
+            return (string.IsNullOrEmpty(propertyName) || 
+                    (_decoderDictionary == null) || 
+                    !_decoderDictionary.ContainsKey(propertyName))
+                ? string.Empty 
+                : _decoderDictionary[propertyName];
+        }
+
+        private static bool SetPath([NotNull] string value,
+            [CanBeNull, CallerMemberName] string propertyName = null)
+        {
+            if (string.IsNullOrEmpty(propertyName)) return false;
+
+            if ((_decoderDictionary != null) &&
+                _decoderDictionary.ContainsKey(propertyName) &&
+                (_decoderDictionary[propertyName] == value))
+            {
+                return false;
+            }
+
+            // build a list of all affected macros
+            var hitList = new List<string>();
+
+            if (_decoderDictionary == null)
+            {
+                _decoderDictionary = new Dictionary<string, string>(MasterDictionary);
+                hitList.AddRange(_decoderDictionary.Keys);  // mark all changed
+            }
+            else
+            {
+                // find which macros will be affected
+                hitList.Add(propertyName);
+
+                for (var modified = true; modified; )
+                {
+                    modified = false;
+
+                    foreach (var kvp in MasterDictionary
+                        .Where(kvp => !hitList.Contains(kvp.Key)))
+                    {
+                        foreach (var key in hitList)
+                        {
+                            var macro = "<" + key + ">";
+
+                            if (kvp.Value.Contains(macro))
+                            {
+                                hitList.Add(kvp.Key);
+                                modified = true;
+                                break;
+                            }
+                        }
+
+                        if (modified) break;
+                    }
+                }
+
+                foreach (var key in hitList
+                    .Where(key => key != propertyName &&
+                                  MasterDictionary.ContainsKey(key)))
+                {
+                    _decoderDictionary[key] = MasterDictionary[key];
+                }
+            }
+
+            // modify the specified value
+            _decoderDictionary[propertyName] = value;
+
+            // make a copy of the hit list for reporting
+            IReadOnlyList<string> notifyList = new List<string>(hitList);
+
+            for (var mod = true; mod; )
+            {
+                mod = false;
+
+                foreach (var key in hitList
+                    .Where(key => !MasterDictionary.ContainsKey(key) ||
+                                  _decoderDictionary[key] != MasterDictionary[key]))
+                {
+                    var macro = "<" + key + ">";
+
+                    for (var modified = true; modified; )
+                    {
+                        modified = false;
+
+                        foreach (var kvp in _decoderDictionary 
+                            .Where(kvp => (kvp.Key != key) && 
+                                          kvp.Value.Contains(macro)))
+                        {
+                            _decoderDictionary[kvp.Key] =
+                                kvp.Value.Replace(macro, _decoderDictionary[key]);
+
+                            mod = modified = true;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return true;
+        }
+
     }
 
     public class DebugTraceListener : TraceListener
